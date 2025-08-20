97ed8cf192619d5fa2ff01edf0b606335a8fa2d8
Boyan Pavlov <tepidentity@gmail.com>
2025-08-20 12:10:33 +0300

    Changes min value for conversion operation to 0.5

--- src/main/java/com/zetta/exchangerates/dto/ConversionDTO.java
+++ src/main/java/com/zetta/exchangerates/dto/ConversionDTO.java
@@ -1,8 +1,8 @@
 package com.zetta.exchangerates.dto;
 
 import com.zetta.exchangerates.entity.Currency;
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 
-public record ConversionDTO(@NotNull Currency sourceCurrency, @NotNull Currency targetCurrency, @Min(0) @NotNull Double amount) {
+public record ConversionDTO(@NotNull Currency sourceCurrency, @NotNull Currency targetCurrency, @DecimalMin("0.5") @NotNull Double amount) {
 }
